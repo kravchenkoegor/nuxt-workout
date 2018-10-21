04ffc694bb2cb800cb7d73eb5898077baf7b9c21
ekravchenko <ESaKravchenko@synergy.ru>
2018-10-21 14:18:53 +0500

    add redirect-ssl

--- api/index.js
+++ api/index.js
@@ -5,9 +5,11 @@ const express = require('express')
 const router = express.Router({})
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const redirectSSL = require('redirect-ssl')
 
 // Server
 const app = express()
+app.use(redirectSSL)
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
@@ -24,7 +26,7 @@ database.connect()
 // console.log(`Server is listening on port ${process.env.PORT || 3000}`)
 
 // Ping
-// setInterval(() => https.get(process.env.BASE_URL), 900000)
+setInterval(() => https.get(process.env.BASE_URL), 900000)
 
 module.exports = {
   path: '/api/',
